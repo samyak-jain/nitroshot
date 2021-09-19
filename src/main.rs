c5db04e8817c10bc89499f3cb5b0595742e66436
samyak-jain <samtan106@gmail.com>
2021-09-19 21:09:06 +0530

    make changes to code to accomodate UI changes

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,45 @@ use crate::screenshot::select::select_screen;
 
 mod screenshot;
 
+#[derive(Debug)]
+enum CaptureType {
+    WINDOW,
+    SCREEN,
+    SELECTION,
+    NONE
+}
+
+impl From<i32> for CaptureType {
+    fn from(number: i32) -> Self {
+        match number {
+            0 => Self::SELECTION,
+            1 => Self::SCREEN,
+            2 => Self::WINDOW,
+            _ => Self::NONE,
+        }
+    }
+}
+
 sixtyfps::include_modules!();
+
 fn main() {
     let window = MainWindow::new();
 
     let weak_window = window.as_weak();
 
-    window.on_start_recording(|capture_type, delay| {
-        println!("Capture Type: {}; Delay: {}", capture_type, delay);
+    window.global::<AppState>().on_start_recording(|| {
+        println!("recording")
     });
 
-    window.on_take_screenshot(move |capture_type, delay| {
-        println!("Capture Type: {}; Delay: {}", capture_type, delay);
-
+    window.global::<AppState>().on_take_screenshot(|| {
         if let Some(strong_window) = weak_window.upgrade() {
-            strong_window.hide();
-            let res = select_screen();
+            let capture_type = CaptureType::from(strong_window.global::<AppState>().get_capture_type());
+            let delay = strong_window.global::<AppState>().get_delay();
 
-            println!("Selected area: {}", res);
+            println!("Capture Type: {:#?}; Delay: {}", capture_type, delay);
+            strong_window.hide();
+
+            select_screen(capture_type, delay);
 
             strong_window.show();
         }
--- src/screenshot/select.rs
+++ src/screenshot/select.rs
@@ -1,6 +1,15 @@
 use hacksaw::HackSawConfig;
 
-pub fn select_screen() -> String {
+use crate::CaptureType;
+
+pub fn select_screen(capture_type: CaptureType, delay: i32) {
+    match capture_type {
+        CaptureType::WINDOW => todo!(),
+        CaptureType::SCREEN => todo!(),
+        CaptureType::SELECTION => todo!(),
+        CaptureType::NONE => return,
+    }
+
     let config = HackSawConfig::default();
     let hacksaw_result = hacksaw::make_selection(Some(config)).unwrap();
 }
