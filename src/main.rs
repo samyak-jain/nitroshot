1208a77b5e59bea2f1907ef6dd7579ede0bedf8c
samyak-jain <samtan106@gmail.com>
2021-10-10 22:58:44 +0530

    minor bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-use crate::screenshot::select::select_screen;
+use crate::screenshot::select::{select_screen, CaptureType};
 
 mod screenshot;
-
-#[derive(Debug)]
-enum CaptureType {
-    WINDOW,
-    SCREEN,
-    SELECTION,
-    NONE
-}
-
-impl From<i32> for CaptureType {
-    fn from(number: i32) -> Self {
-        match number {
-            0 => Self::SELECTION,
-            1 => Self::SCREEN,
-            2 => Self::WINDOW,
-            _ => Self::NONE,
-        }
-    }
-}
 
 sixtyfps::include_modules!();
 
@@ -28,13 +9,14 @@ fn main() {
 
     let weak_window = window.as_weak();
 
-    window.global::<AppState>().on_start_recording(|| {
-        println!("recording")
-    });
+    window
+        .global::<AppState>()
+        .on_start_recording(|| println!("recording"));
 
-    window.global::<AppState>().on_take_screenshot(|| {
+    window.global::<AppState>().on_take_screenshot(move || {
         if let Some(strong_window) = weak_window.upgrade() {
-            let capture_type = CaptureType::from(strong_window.global::<AppState>().get_capture_type());
+            let capture_type =
+                CaptureType::from(strong_window.global::<AppState>().get_capture_type());
             let delay = strong_window.global::<AppState>().get_delay();
 
             println!("Capture Type: {:#?}; Delay: {}", capture_type, delay);
--- src/screenshot/select.rs
+++ src/screenshot/select.rs
@@ -1,15 +1,33 @@
 use hacksaw::HackSawConfig;
 
-use crate::CaptureType;
+#[derive(Debug)]
+pub enum CaptureType {
+    WINDOW,
+    SCREEN,
+    SELECTION,
+    NONE,
+}
+
+impl From<i32> for CaptureType {
+    fn from(number: i32) -> Self {
+        match number {
+            0 => Self::SELECTION,
+            1 => Self::SCREEN,
+            2 => Self::WINDOW,
+            _ => Self::NONE,
+        }
+    }
+}
 
 pub fn select_screen(capture_type: CaptureType, delay: i32) {
+    let config = HackSawConfig::default();
+
     match capture_type {
         CaptureType::WINDOW => todo!(),
         CaptureType::SCREEN => todo!(),
-        CaptureType::SELECTION => todo!(),
+        CaptureType::SELECTION => {
+            let hacksaw_result = hacksaw::make_selection(Some(config)).unwrap();
+        }
         CaptureType::NONE => return,
     }
-
-    let config = HackSawConfig::default();
-    let hacksaw_result = hacksaw::make_selection(Some(config)).unwrap();
 }
